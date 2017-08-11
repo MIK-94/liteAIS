838a25b00278725a4f6564924da9fb0e08aeef84
Михаил <wild8.panda66@yandex.ru>
2017-08-11 21:00:21 +0700

    Update table_cahge.php

--- include/table_cahge.php
+++ include/table_cahge.php
@@ -1,11 +1,8 @@
 <?php 
-  session_start(); 
-//$new_table = 'tovar'; 
+session_start(); 
 if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
-$new_table = $_POST['table_name'];  
-
-  $_SESSION['table'] = $new_table;
+    $new_table = $_POST['table_name'];  
+    $_SESSION['table'] = $new_table;
   echo " Таблица ".$new_table." выбрана";
 } 
-
 ?>
