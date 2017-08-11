a5d0f5f04c0ae5e45e9674bb44e1452694598e9c
Михаил <wild8.panda66@yandex.ru>
2017-08-11 22:17:54 +0700

    Update show_tables.php

--- show_tables.php
+++ show_tables.php
@@ -5,9 +5,10 @@ require_once("include/con.php");
 echo "<label for='select_table'>Выбор таблиц: </label><br>";
 echo "<select id='select_table' class='btn btn-default'>";
 $id=0;
-$res = $con->query("SHOW TABLES FROM id2516673_torg");
+$res = $con->query("SHOW TABLES FROM torg");
 while ($row=$res->fetch_assoc()) {
-	echo "<option value='".$row["Tables_in_id2516673_torg"]."'>".$row["Tables_in_id2516673_torg"]."</option>";
+	#ОТредактировать наименование БД в Tables_in_torg
+	echo "<option value='".$row["Tables_in_torg"]."'>".$row["Tables_in_torg"]."</option>";
 	}
 echo "</select>  </div> <button type='button' class='btn btn-default' onclick='table_change()'>Принять</button>";
 ?>
